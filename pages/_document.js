bc4c5316834554142d5609eb0b3f5b6b2548da41
Ben Hadfield <benjohnhadfield@gmail.com>
2017-10-08 19:33:45 +0100

    add fav

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,9 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
+          <title>Blockchain Project</title>
           <link rel='manifest' href='/static/manifest.json' />
+          <link rel='icon' href='/static/fav.png' />
           <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' />
           <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css' />
         </Head>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import Grid from 'material-ui/Grid'
-import Paper from 'material-ui/Paper'
 import withNav from '../enhancers/withNav'
 import Overview from '../components/overview'
 
